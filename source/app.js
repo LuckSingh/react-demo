76280ab215f051ea953a6d477fe24f0944c25352
enterpress <xinge.tong@bluegogo.com>
2017-07-19 19:24:47 +0800

    添加基本的配置文件

---
+++ source/app.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import ReactDom from 'react-dom';
+import TodoList from './todoList';
+
+ReactDom.render(
+    <TodoList todos={['react-starter', 'angular-starter', 'vue-starter']}/>,
+    document.getElementById('container')
+);
---
+++ source/todoItems.js
@@ -0,0 +1,21 @@
+
+import React from 'react';
+
+export default class TodoItems extends React.Component {
+    static propTypes = {
+        items: React.PropTypes.array
+    };
+
+    constructor(props) {
+        super(props);
+    }
+
+    render() {
+        const createItem = function(item, index) {
+            return (
+                <li key={index}>{item}</li>
+            );
+        };
+        return <ul>{this.props.items.map(createItem)}</ul>;
+    }
+}
