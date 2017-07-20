6c584103bf6110df8f650732177b8ad30dc3edfd
LessTong <tongxinge108@163.com>
2017-07-20 09:29:14 +0800

    aa

--- src/app.js
+++ src/app.js
@@ -1,16 +1,18 @@
 import React from 'react';
 import ReactDom from 'react-dom';
 import {
-	Router,
-	Route,
-	hashHistory
-} from 'react-router'
-//import routers from './routers.js';
+	BrowserRouter as Router,
+	Route
+} from 'react-router-dom';
+import createBrowserHistory from 'history/createBrowserHistory'
+
+const customHistory = createBrowserHistory()
+	//import routers from './routers.js';
 import App from './main/app/index';
 import Coupons from './main/coupons/index';
 import Cancellation from './main/cancellation/index';
 ReactDom.render(
-	<Router history={hashHistory}>
+	<Router history={customHistory}>
     <Route path="/" component={App}/>
     <Route path="/coupons" component={Coupons}/>
     <Route path="/cancellation" component={Cancellation}/>
